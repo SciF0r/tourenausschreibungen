0c88146b29a298e6f0fbc9e9f86f645a88e24783
Denis Simonet <denis.simonet@bluewin.ch>
2019-03-20 19:53:20 +0100

    Continue parsing

--- tour_parser.py
+++ tour_parser.py
@@ -6,16 +6,18 @@ class TourParser(object):
     """Parse tours retrieved from the SAC Aarau tour page"""
 
     def __init__(self, url, startDate, endDate):
-        self.tours = {}
         self.__read_page(url, startDate, endDate)
 
     def parse(self):
+        """Parse the tours table and return a tours object"""
+        self.__tours = {}
         table = self.__page.find('table', class_='program')
         for row in table.find_all('tr'):
-            print(row.prettify())
+            self.__process_row(row)
+        return self.__tours;
 
     def __read_page(self, url, start_date, end_date):
-        """Returns a BeautifulSoup object of the given url"""
+        """Store a BeautifulSoup object of the given url"""
         data = {
             'start': start_date,
             'end': end_date,
@@ -27,3 +29,9 @@ class TourParser(object):
             data=urllib.parse.urlencode(data).encode("utf-8")
         ) as page:
             self.__page = bs4.BeautifulSoup(page, 'html5lib')
+
+    def __process_row(self, row):
+        """Process a row and return a tours object with its contents"""
+        left, right = row.find_all('td')
+        print(left.prettify())
+        print(right.prettify())
