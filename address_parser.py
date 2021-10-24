20949e0b0f3afe0758fa73db7dc237516d3ff07e
Denis Simonet <denis.simonet@bluewin.ch>
2021-10-24 11:03:46 +0200

    Add parsing for addresses

---
+++ address_parser.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+class AddressParser(object):
+    """Parse addresses retrieved from xls file prepared by someone(TM)"""
+    COL_FUNCTION = 'Funktion'
+    COL_COMMITTEE = 'Gremium'
+    COL_FIRST_NAME = 'Vorname'
+    COL_LAST_NAME = 'Name'
+    COL_ADDRESS = 'Strasse, Nr.'
+    COL_ZIP = 'PLZ'
+    COL_TOWN = 'Ort'
+    COL_EMAIL = 'E-Mail'
+    COL_PHONE_PRIVATE = 'Telefon P'
+    COL_PHONE_BUSINESS = 'Telefon G'
+    COL_PHONE_MOBILE = 'Mobile'
+
+    def __init__(self, file_path):
+        self.__cols_addresses = [
+                self.COL_FUNCTION,
+                self.COL_COMMITTEE,
+                self.COL_FIRST_NAME,
+                self.COL_LAST_NAME,
+                self.COL_ADDRESS,
+                self.COL_ZIP,
+                self.COL_TOWN,
+                self.COL_EMAIL,
+                self.COL_PHONE_PRIVATE,
+                self.COL_PHONE_BUSINESS,
+                self.COL_PHONE_MOBILE
+        ]
+        self.__read_file(file_path)
+
+    def parse_for_year_program(self):
+        """Parse the addresses table and return an addresses object"""
+        return pd.DataFrame(self.__data, columns = self.__cols_addresses)
+
+    def __read_file(self, file_path):
+        """Store an object with the xls file content"""
+        data = pd.read_csv(file_path, sep = ';')
+        data[self.COL_ZIP] = data[self.COL_ZIP].astype(pd.Int64Dtype())
+        self.__data = data
---
+++ collect_addresses.py
@@ -0,0 +1,18 @@
+import sys
+from datetime import datetime
+from docx_creator_addresses import DocxCreator
+from address_parser import AddressParser
+
+if len(sys.argv) != 2:
+    print('Usage: {0} path_to_xlsx_file'.format(sys.argv[0]))
+    sys.exit(1)
+
+file_path = sys.argv[1]
+
+print('Getting tours from {0}'.format(file_path))
+parser = AddressParser(file_path)
+addresses = parser.parse_for_year_program()
+
+export_file_name = '{0}_adressen.docx'.format(datetime.now().strftime('%Y-%m-%d_%H_%M_%S'))
+docx_creator = DocxCreator(addresses, export_file_name)
+docx_creator.create()
