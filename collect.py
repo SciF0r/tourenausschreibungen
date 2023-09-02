ce398486b233f7c59f6829c9869e8bf88b7b979a
Denis Simonet <denis.simonet@bluewin.ch>
2023-09-02 10:10:08 +0200

    Extend parser to support rote karte

--- collect.py
+++ collect.py
@@ -4,21 +4,27 @@ from docx_creator import DocxCreator
 from tour_cleaner import TourCleaner
 from tour_parser import TourParser
 
-if len(sys.argv) != 2:
-    print('Usage: {0} path_to_xlsx_file'.format(sys.argv[0]))
+if len(sys.argv) != 3:
+    print('Usage: {0} jahresprogramm|rotekarte path_to_xlsx_file'.format(sys.argv[0]))
     sys.exit(1)
 
-file_path = sys.argv[1]
+parser_type = sys.argv[1].lower()
+file_path = sys.argv[2]
 
 print('Getting tours from {0}'.format(file_path))
+
+if parser_type not in ['jahresprogramm', 'rotekarte']:
+    print('Usage: {0} jahresprogramm|rotekarte path_to_xlsx_file'.format(sys.argv[0]))
+    sys.exit(1)
+
 parser = TourParser(file_path)
-tours = parser.parse_for_year_program()
+tours = parser.parse(parser_type)
 
 cleaner = TourCleaner(tours)
 with open('rules.txt', 'r') as rules_file:
     rules = [line.split('/', 2) for line in rules_file.read().splitlines()]
 cleaned_tours = cleaner.clean(rules)
 
-export_file_name = '{0}_jahresprogramm_{1}.docx'.format(datetime.now().strftime('%Y-%m-%d_%H_%M_%S'), '{0}')
+export_file_name = '{0}_{1}_{2}.docx'.format(datetime.now().strftime('%Y-%m-%d_%H_%M_%S'), parser_type, '{0}')
 docx_creator = DocxCreator(cleaned_tours, export_file_name)
 docx_creator.create()
--- tour_parser.py
+++ tour_parser.py
@@ -4,13 +4,29 @@ class TourParser(object):
     """Parse tours retrieved from a DropTours xlsx export file"""
     COL_GROUP = 'Gruppe'
     COL_START_DATE = 'Startdatum'
+    COL_END_DATE = 'Enddatum'
     COL_DURATION = 'Dauer'
     COL_TOUR_TYPE = 'Tourtyp'
+    COL_TOUR_TYPE_LONG = 'Tourtyp lang'
+    COL_GUIDE = 'Tourenleiter 1'
+    COL_METRICS = 'Auf-, Abstieg/Marschzeit'
+    COL_ROUTE = 'Reiseroute'
+    COL_HOSPITALITY = 'Unterkunft / Verpflegung'
+    COL_COSTS = 'Kosten'
+    COL_COSTS_LONG = 'Kosten lang'
+    COL_EXECUTION = 'Durchführungskontakt'
+    COL_MEETING_POINT = 'Treffpunkt'
+    COL_DESCRIPTION = 'Routenbeschreibung'
+    COL_GEAR = 'Ausrüstung'
+    COL_REGISTRATION_MEANS = 'Anmeldung'
+    COL_REGISTRATION_START_DATE = 'Anmeldestart'
+    COL_REGISTRATION_END_DATE = 'Anmeldeschluss'
     COL_COND_REQ = 'Kond. Anforderungen'
     COL_TECH_REQ = 'Techn. Anforderungen'
     COL_ACTIVITY = 'Aktivität'
     COL_FIRST_NAME = 'Vorname'
     COL_LAST_NAME = 'Name'
+    COL_STATUS = 'Status'
     EVENT_TYPES = ['Ftn', 'Div']
 
 
@@ -24,15 +40,41 @@ class TourParser(object):
                 self.COL_TECH_REQ,
                 self.COL_ACTIVITY,
                 self.COL_FIRST_NAME,
-                self.COL_LAST_NAME
+                self.COL_LAST_NAME,
+        ]
+        self.__cols_rote_karte = [
+                self.COL_GROUP,
+                self.COL_STATUS,
+                self.COL_START_DATE,
+                self.COL_END_DATE,
+                self.COL_ACTIVITY,
+                self.COL_TOUR_TYPE_LONG,
+                self.COL_GUIDE,
+                self.COL_COND_REQ,
+                self.COL_TECH_REQ,
+                self.COL_METRICS,
+                self.COL_ROUTE,
+                self.COL_HOSPITALITY,
+                self.COL_COSTS,
+                self.COL_COSTS_LONG,
+                self.COL_EXECUTION,
+                self.COL_MEETING_POINT,
+                self.COL_DESCRIPTION,
+                self.COL_GEAR,
+                self.COL_REGISTRATION_MEANS,
+                self.COL_REGISTRATION_START_DATE,
+                self.COL_REGISTRATION_END_DATE,
         ]
         self.__read_file(file_path)
 
-    def parse_for_year_program(self):
+    def parse(self, parser_type):
         """Parse the tours table and return a tours object"""
         self.__data[self.COL_GROUP] = self.__data.apply(lambda tour: self.__get_real_group(tour[self.COL_GROUP], tour[self.COL_TOUR_TYPE]), axis = 1)
         self.__data.sort_values([self.COL_START_DATE, self.COL_TOUR_TYPE], inplace = True)
-        return pd.DataFrame(self.__data, columns = self.__cols_year_program)
+        if parser_type == 'jahresprogramm':
+            return pd.DataFrame(self.__data, columns = self.__cols_year_program)
+        elif parser_type == 'rotekarte':
+            return pd.DataFrame(self.__data, columns = self.__cols_rote_karte)
 
     def __read_file(self, file_path):
         """Store an object with the xls file content"""
